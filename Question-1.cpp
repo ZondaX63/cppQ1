ad24fe9467b9936f1681b36f39c18013800a54b2
ZondaX <61884303+ZondaX63@users.noreply.github.com>
2022-04-06 16:17:25 +0300

    Update Question-1.cpp

--- Question-1.cpp
+++ Question-1.cpp
@@ -1,40 +1,15 @@
 #include <iostream>
-
 using namespace std;
-
-bool CheckDigitDiff(int);
-
 int main()
 {
     int number;
-    bool a;
-    cout << "Give me a number(with 3 digit): ";
-    
+    int i = 1;
+    cout << "Number: ";
     cin >> number;
-    if (CheckDigitDiff(number))
+    while (i <= number)
     {
-        cout << "The Digits of Number is not same";
-    }
-
-    else
-    {
-        cout << "The Digits of Number is same";
-    }
-}
-bool CheckDigitDiff(int num)
-{
-    int a, b, c;
-    a = num / 100;
-    num = num - (a * 100);
-    b = num / 10;
-    num = num - (b * 10);
-    c = num;
-    if (a == b || a == c || c == b)
-    {
-        return false;
-    }
-    else
-    {
-        return true;
+        if (number % i == 0)
+            cout << " " << i;
+        i++;
     }
 }
